653b7242dceb1c425fa8aebea3542ee0ad28b96e
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-31 01:57:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ let driver = {}
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver,{[key]: value});
 };
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
+}
