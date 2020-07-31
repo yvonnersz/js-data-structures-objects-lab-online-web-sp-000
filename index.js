149b70137c1d12c76d9e5fc41172803bec4130bf
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-31 02:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function deleteFromDriverByKey(driver, key) {
   delete newDriver[key]
   return newDriver;
 }
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+}
